e492af617a83cc5e0b697e7b69fd118f273fbcc8
Edgard Reykh <68498236+Noothing@users.noreply.github.com>
2024-06-10 17:39:35 +0400

    init

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,17 @@
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
-    const res = await fetch('https://api.noothing.xyz/me')
-    const json = await res.json()
-    console.log(json)
+    fetch('https://api.noothing.xyz/me', {
+        method: 'GET', // or 'POST' if the route requires a POST request
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer YOUR_ACCESS_TOKEN' // Add this line if the request requires authorization
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
